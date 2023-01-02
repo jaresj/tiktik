714f7563b4d18b8008371d0562fc5f52b66af030
jaresj <justice.jares@gmail.com>
2023-01-02 09:41:55 -0800

    mvp for tiktok clone

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: ["pixabay.com", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
